8c72fe63cca3b7d48c1713b3c08347d4946e2678
Lakshya sharma <lokmanh2148t@gmail.com>
2025-12-27 22:57:08 +0530

    query middleware

--- models/tourModels.js
+++ models/tourModels.js
@@ -54,6 +54,10 @@ const tourSchema = new mongoose.Schema({
   startDates: {
     type: [Date],
   },
+  secretTour:{
+    type:Boolean,
+    default:false 
+  }
 },
 {
 toJSON:{virtuals:true},
@@ -73,16 +77,30 @@ this.slug = slugify(this.name,{lower:true});
 next();
 })
 
-tourSchema.pre('save', function (next) {
-  console.log("will save document");
-  next();
-});
+// tourSchema.pre('save', function (next) {
+//   console.log("will save document"); 
+//   next();
+// });
 
-tourSchema.post('save',function(doc,next){
-console.log(doc);
-next();
+// tourSchema.post('save',function(doc,next){
+// console.log(doc);
+// next();
+// })
+
+// Query middleware
+
+// tourSchema.pre('find',function(next){
+tourSchema.pre(/^find/,function(next){
+this.find({secretTour:{$ne:true}})
+this.start = Date.now();
+  next();
 })
 
+tourSchema.post(/^find/, function (docs,next) {
+  console.log(`Query took ${Date.now()-this.start}miliseconds`);
+  console.log(docs)
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
